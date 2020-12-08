daeee6f487faa574aa10c8736e22fb4186993f22
unknown <yarden@DESKTOP-J54ES58.com>
2020-12-08 19:35:17 +0200

    Final CV by Yarden Hochenberg

---
+++ CV_hochyard/Yarden Hochenberg CV.js
@@ -0,0 +1,29 @@
+
+function openningFunction() {
+
+    var modal = document.getElementById("myModal");
+
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+// form
+function thankyouFunction(){
+    var o = document.forms["frm"]["PhoneNum"].value;
+    if (o!="") {
+       alert("Thank you for trying to contact me! I will get back to you soon. Yarden") 
+    }
+    
+}
+
